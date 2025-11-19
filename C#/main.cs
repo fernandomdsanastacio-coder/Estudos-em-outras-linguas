fc62de5275fbdc4f4acb98b3a7eef5f5af09ac3b
Spark_zero <fernando.m.d.s.anastacio@gmail.com>
2025-11-19 18:26:00 +0000

    Add files via upload

---
+++ C#/main.cs
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+                            Online C# Compiler.
+                Code, Compile, Run and Debug C# program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+using System;
+char num=':';
+char num1=')';
+for (int i=0; i<10; i++)
+{
+
+Console.Write(num);
+Console.WriteLine(num1);
+}
