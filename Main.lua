ba708cf79293111b0a9bbd3288c562f1a536e964
Spark_zero <fernando.m.d.s.anastacio@gmail.com>
2025-11-19 18:05:05 +0000

    Add files via upload

---
+++ Main.lua
@@ -0,0 +1,14 @@
+local text = "Boa tarde"
+local num = 117 
+
+function numtext()                                         
+     print(text)
+     return 
+end
+    function hello()
+      print (num)
+end
+
+
+numtext()
+hello()
